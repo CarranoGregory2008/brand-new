9018aca521d9324211421bf70297add5faa19893
CarranoGregory2008 <167574542+CarranoGregory2008@users.noreply.github.com>
2024-11-14 01:46:05 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -6,7 +6,7 @@ do
   echo "Memulai proses menambang..."
   ore \
     --rpc https://wider-maximum-log.solana-mainnet.quiknode.pro/mktp-f6a2093b42b36d48a08ede7587340653c0407706 \
-    --keypair /brand-new/id.json \
+    --keypair id.json \
     --priority-fee 5000000 \
     mine \
     --threads 96 # ubah sesuai prosesor server kamu.
