9bc8949ca2f28b5dd13cd773617e41e1b436a0b8
CarranoGregory2008 <167574542+CarranoGregory2008@users.noreply.github.com>
2024-11-14 02:28:22 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -6,7 +6,7 @@ do
   echo "Memulai proses menambang..."
   ore \
     --rpc https://api.mainnet-beta.solana.com \
-    --keypair id.json \
+    --keypair /brand-new/id.json \
     --priority-fee 1 \
     mine \
     --threads 96 # ubah sesuai prosesor server kamu.
